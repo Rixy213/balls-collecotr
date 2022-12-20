141ebf5696da24b3966345598e83638d8a42dd33
Rixy213 <109170623+Rixy213@users.noreply.github.com>
2022-12-20 17:42:45 +0100

    Add files via upload

---
+++ ballsdex/core/bot.py
@@ -0,0 +1,239 @@
+from __future__ import annotations
+
+import discord
+import logging
+
+from rich import print
+from typing import cast
+from datetime import datetime
+
+from discord import app_commands
+from discord.ext import commands
+
+from ballsdex.core.dev import Dev
+from ballsdex.core.models import BlacklistedID, Special
+from ballsdex.core.commands import Core
+
+log = logging.getLogger("ballsdex.core.bot")
+
+PACKAGES = ["config", "players", "countryballs", "info"]
+
+
+def owner_check(ctx: commands.Context[BallsDexBot]):
+    return ctx.bot.is_owner(ctx.author)
+
+
+class CommandTree(app_commands.CommandTree):
+    async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
+        bot = cast(BallsDexBot, interaction.client)
+        return await bot.blacklist_check(interaction)
+
+
+class BallsDexBot(commands.AutoShardedBot):
+    """
+    BallsDex Discord bot
+    """
+
+    def __init__(self, command_prefix: str, dev: bool = False, **options):
+        # An explaination for the used intents
+        # guilds: needed for basically anything, the bot needs to know what guilds it has
+        # and accordingly enable automatic spawning in the enabled ones
+        # guild_messages: spawning is based on messages sent, content is not necessary
+        # emojis_and_stickers: DB holds emoji IDs for the balls which are fetched from 3 servers
+        intents = discord.Intents(guilds=True, guild_messages=True, emojis_and_stickers=True)
+
+        super().__init__(command_prefix, intents=intents, tree_cls=CommandTree, **options)
+        self._shutdown = 0
+        self.dev = dev
+        self.tree.error(self.on_application_command_error)
+        self.blacklist: list[int] = []
+        self.special_cache: list[Special] = []
+        self.add_check(owner_check)  # Only owners are able to use text commands
+
+    async def on_shard_ready(self, shard_id: int):
+        log.debug(f"Connected to shard #{shard_id}")
+
+    def assign_ids_to_app_groups(
+        self, group: app_commands.Group, synced_commands: list[app_commands.AppCommandGroup]
+    ):
+        for synced_command in synced_commands:
+            bot_command = group.get_command(synced_command.name)
+            if not bot_command:
+                continue
+            bot_command.extras["mention"] = synced_command.mention
+            if isinstance(bot_command, app_commands.Group) and bot_command.commands:
+                self.assign_ids_to_app_groups(
+                    bot_command, cast(list[app_commands.AppCommandGroup], synced_command.options)
+                )
+
+    def assign_ids_to_app_commands(self, synced_commands: list[app_commands.AppCommand]):
+        for synced_command in synced_commands:
+            bot_command = self.tree.get_command(synced_command.name, type=synced_command.type)
+            if not bot_command:
+                continue
+            bot_command.extras["mention"] = synced_command.mention
+            if isinstance(bot_command, app_commands.Group) and bot_command.commands:
+                self.assign_ids_to_app_groups(
+                    bot_command, cast(list[app_commands.AppCommandGroup], synced_command.options)
+                )
+
+    async def load_blacklist(self):
+        self.blacklist = (
+            await BlacklistedID.all().only("discord_id").values_list("discord_id", flat=True)
+        )  # type: ignore
+
+    async def load_special_cache(self):
+        now = datetime.now()
+        self.special_cache = await Special.filter(start_date__lte=now, end_date__gt=now)
+
+    async def on_ready(self):
+        assert self.user
+        log.info(f"Successfully logged in as {self.user} ({self.user.id})!")
+
+        # set bot owners
+        assert self.application
+        if self.application.team:
+            self.owner_ids = [m.id for m in self.application.team.members]
+            log.info(
+                f"{len(self.owner_ids)} users from team {self.application.team.name} "
+                "are set as bot owners."
+            )
+        else:
+            self.owner_id = self.application.owner.id
+            log.info(f"{self.application.owner} is set as the bot owner.")
+
+        await self.load_blacklist()
+        await self.load_special_cache()
+        if self.blacklist:
+            log.info(f"{len(self.blacklist)} blacklisted users.")
+
+        log.info("Loading packages...")
+        await self.add_cog(Core(self))
+        if self.dev:
+            await self.add_cog(Dev())
+        loaded_packages = []
+        for package in PACKAGES:
+            try:
+                await self.load_extension("ballsdex.packages." + package)
+            except Exception:
+                log.error(f"Failed to load package {package}", exc_info=True)
+            else:
+                loaded_packages.append(package)
+        if loaded_packages:
+            log.info(f"Packages loaded: {', '.join(loaded_packages)}")
+        else:
+            log.info("No package loaded.")
+        synced_commands = await self.tree.sync()
+        if synced_commands:
+            log.info(f"Synced {len(synced_commands)} commands.")
+            try:
+                self.assign_ids_to_app_commands(synced_commands)
+            except Exception:
+                log.error("Failed to assign IDs to app commands", exc_info=True)
+        else:
+            log.info("No command to sync.")
+        print("\n    [bold][red]BallsDex bot[/red] [green]is now operational![/green][/bold]\n")
+
+    async def blacklist_check(self, interaction: discord.Interaction) -> bool:
+        if interaction.user.id in self.blacklist:
+            await interaction.response.send_message(
+                "You are blacklisted from the bot.", ephemeral=True
+            )
+            return False
+        return True
+
+    async def on_command_error(
+        self, context: commands.Context, exception: commands.errors.CommandError
+    ):
+        if isinstance(
+            exception, (commands.CommandNotFound, commands.CheckFailure, commands.DisabledCommand)
+        ):
+            return
+
+        assert context.command
+        if isinstance(exception, (commands.ConversionError, commands.UserInputError)):
+            # in case we need to know what happened
+            log.debug("Silenced command exception", exc_info=exception)
+            await context.send_help(context.command)
+            return
+
+        if isinstance(exception, commands.MissingRequiredAttachment):
+            await context.send("An attachment is missing.")
+            return
+
+        if isinstance(exception, commands.CommandInvokeError):
+
+            if isinstance(exception.original, discord.Forbidden):
+                await context.send("The bot does not have the permission to do something.")
+                # log to know where permissions are lacking
+                log.warning(
+                    f"Missing permissions for text command {context.command.name}",
+                    exc_info=exception.original,
+                )
+                return
+
+            log.error(f"Error in text command {context.command.name}", exc_info=exception.original)
+            await context.send(
+                "An error occured when running the command. Contact support if this persists."
+            )
+            return
+
+        await context.send(
+            "An error occured when running the command. Contact support if this persists."
+        )
+        log.error(f"Unknown error in text command {context.command.name}", exc_info=exception)
+
+    async def on_application_command_error(
+        self, interaction: discord.Interaction, error: app_commands.AppCommandError
+    ):
+        async def send(content: str):
+            if interaction.response.is_done():
+                await interaction.followup.send(content, ephemeral=True)
+            else:
+                await interaction.response.send_message(content, ephemeral=True)
+
+        if isinstance(error, app_commands.CheckFailure):
+            await send("You are not allowed to use that command.")
+            return
+
+        if isinstance(error, app_commands.CommandInvokeError):
+            assert interaction.command
+
+            if isinstance(error.original, discord.Forbidden):
+                await send("The bot does not have the permission to do something.")
+                # log to know where permissions are lacking
+                log.warning(
+                    f"Missing permissions for app command {interaction.command.name}",
+                    exc_info=error.original,
+                )
+                return
+
+            if isinstance(error.original, discord.InteractionResponded):
+                # most likely an interaction received twice (happens sometimes),
+                # or two instances are running on the same token.
+                log.warning(
+                    f"Tried invoking command {interaction.command.name}, but the "
+                    "interaction was already responded to.",
+                    exc_info=error.original,
+                )
+                # still including traceback because it may be a programming error
+
+            log.error(
+                f"Error in slash command {interaction.command.name}", exc_info=error.original
+            )
+            await send(
+                "An error occured when running the command. Contact support if this persists."
+            )
+            return
+
+        await send("An error occured when running the command. Contact support if this persists.")
+        log.error("Unknown error in interaction", exc_info=error)
+
+    async def on_error(self, event_method: str, /, *args, **kwargs):
+        formatted_args = ", ".join(args)
+        formatted_kwargs = " ".join(f"{x}={y}" for x, y in kwargs.items())
+        log.error(
+            f"Error in event {event_method}. Args: {formatted_args}. Kwargs: {formatted_kwargs}",
+            exc_info=True,
+        )
+        self.tree.interaction_check
---
+++ ballsdex/core/commands.py
@@ -0,0 +1,78 @@
+import logging
+
+from typing import TYPE_CHECKING
+from discord.ext import commands
+
+log = logging.getLogger("ballsdex.core.commands")
+
+if TYPE_CHECKING:
+    from .bot import BallsDexBot
+
+
+class Core(commands.Cog):
+    """
+    Core commands of BallsDex bot
+    """
+
+    def __init__(self, bot: "BallsDexBot"):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        """
+        Ping!
+        """
+        await ctx.send("Pong")
+
+    @commands.command()
+    @commands.is_owner()
+    async def reloadtree(self, ctx: commands.Context):
+        """
+        Sync the application commands with Discord
+        """
+        await self.bot.tree.sync()
+        await ctx.send("Application commands tree reloaded.")
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx: commands.Context, package: str):
+        """
+        Reload an extension
+        """
+        package = "ballsdex.packages." + package
+        try:
+            try:
+                await self.bot.reload_extension(package)
+            except commands.ExtensionNotLoaded:
+                await self.bot.load_extension(package)
+        except commands.ExtensionNotFound:
+            await ctx.send("Extension not found")
+        except Exception:
+            await ctx.send("Failed to reload extension.")
+            log.error(f"Failed to reload extension {package}", exc_info=True)
+        else:
+            await ctx.send("Extension reloaded.")
+
+    @commands.command()
+    @commands.is_owner()
+    async def reloadblacklist(self, ctx: commands.Context):
+        """
+        Reload the cache of blacklisted users.
+
+        This is needed each time the database is updated, otherwise changes won't reflect until
+        next start.
+        """
+        await self.bot.load_blacklist()
+        await ctx.message.add_reaction("✅")
+
+    @commands.command()
+    @commands.is_owner()
+    async def reloadspecial(self, ctx: commands.Context):
+        """
+        Reload the cache of special balls (events and backgrounds).
+
+        This is needed each time the database is updated, otherwise changes won't reflect until
+        next start.
+        """
+        await self.bot.load_special_cache()
+        await ctx.message.add_reaction("✅")
